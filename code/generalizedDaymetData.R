3d70c3231c6e402dae1911a29cc8272b490775d7
awang03cchi <andruwuywang@gmail.com>
2025-06-05 14:05:44 -0400

    Ultimate Generalized thing
    Longer name version still generates the map, shorter removed that. 
     - Takes arguments of State, County, and Date, stores the resulting min, max, mean in "output"
     - Now will not redownload every time, if the file already exists, it will now not redownload and just use the existing one.
     - Yes its now specific to counties
     - COUNTIES WITH CITY VARIANTS MUST BE SPECIFIED e.g. Baltimore City vs Baltimore County ((Baltimore would not work, nor does Baltimore (C)

---
+++ code/generalizedDaymetData.R
@@ -0,0 +1,94 @@
+install.packages(c("terra", "sf", "tigris", "httr", "stringi", "lubridate"))
+
+library(terra)
+library(sf)
+library(tigris)
+library(httr)
+library(stringi)
+library(lubridate)
+
+#Lets let the file download slap it in the same file
+setwd(getwd())
+if (!dir.exists("Daymet")) {dir.create("Daymet")}
+options(tigris_use_cache = TRUE)
+
+# info you input =w=
+currDate = date
+inpState = state
+inpCounty = county
+# Found the bounding boxes at https://observablehq.com/@rdmurphy/u-s-state-bounding-boxes,
+# it is in the format West, South, East, North
+
+# Pasted the bounding boxes to a dataframe then cut up them to a usable
+# format for the scraping link
+boundingPaste$VIRGINIA = "-83.67570908179081,36.540885157941574,-75.24086819838197,39.46598340442404"
+boundingPaste$MARYLAND = "-79.48700299202991,37.91709878227782,-75.05063561675617,39.72284225191251"
+boundingPaste$DC = "-77.11806895668957,38.79162154730547,-76.90988990509905,38.99435963428633"
+dirList = stri_split_fixed(boundingPaste[[inpState]], ",")
+nBound = dirList[[1]][4]
+wBound = dirList[[1]][1]
+sBound = dirList[[1]][2]
+eBound = dirList[[1]][3]
+
+
+
+#SCRAPPING PART TAKES SOME TIME
+# Uses ncss server
+base_url <- "https://thredds.daac.ornl.gov/thredds/ncss/ornldaac/2129"
+
+# Originally used multiple dates but this should be fine
+for (date in currDate) {
+  # Splitting date into usable chunks for link
+  year = substr(date, start = 1, stop = 4)
+  month = substr(date, start = 6, stop = 7)
+  day = substr(date, start = 9, stop = 10)
+  earlyDate = as.Date(date) - 2
+  earlyyear = substr(earlyDate, start = 1, stop = 4)
+  earlymonth = substr(earlyDate, start = 6, stop = 7)
+  earlyday = substr(earlyDate, start = 9, stop = 10)
+  
+  # Have some funny link documentation =w= https://github.com/ornldaac/gridded_subset_example_script
+  # This link is why I cut up all those strings and dates
+  dest <- file.path("Daymet", paste0("daymet_prcp_", date, "_", inpCounty, ".nc"))
+  
+  if (!file.exists(dest)) {
+    url <- paste0(base_url, "/daymet_v4_daily_na_prcp_", year, ".nc?var=lat&var=lon&var=prcp&north=", 
+                  nBound, "&west=", wBound, "&east=", eBound, "&south=", sBound,
+                  "&disableProjSubset=on&horizStride=1&time_start=", earlyyear, "-", 
+                  earlymonth, "-", earlyday, "T00:00:00Z&time_end=", year, "-", month, "-", 
+                  day, "T23:59:59Z&timeStride=1")
+    
+    message("Downloading: ", url)
+    GET(url, write_disk(dest, overwrite = TRUE), progress())
+  } else {
+    message("File already exists: ", dest)
+  }
+}
+
+
+# Calculate
+for (date in currDate) {
+  
+  # Load Daymet raster
+  raster_file <- paste0("Daymet/daymet_prcp_", date, "_", inpCounty, ".nc")
+  r <- rast(raster_file)
+  
+  # Get VA boundary and reproject to match raster CRS
+  co_shape <- county_subdivisions(state = inpState, county = inpCounty, cb = TRUE, class = "sf")
+  co_shape_proj <- st_transform(co_shape, crs(r))
+  co_vect <- vect(co_shape_proj)
+  
+  # Clip raster to VA
+  r_co <- crop(r, co_vect) |> mask(co_vect)
+  
+  # Compute min, max, and mean for each day
+  stats <- global(r_co, fun = c("min", "max", "mean"), na.rm = TRUE)
+  
+  
+  # Use the transposed matrix directly
+  observed_matrix <-as.data.frame(stats)
+  
+  # output is in form min, max, mean
+  output <- c(min(observed_matrix[, "min"]), max(observed_matrix[, "max"]), mean(observed_matrix[, "mean"]))
+  print(output)
+}
